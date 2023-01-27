d541d961ed326315a318d2f3d3a2199528c251fd
Manzi Cedrick <cedrickmanzii0@gmail.com>
2023-01-27 13:43:38 +0200

    [add] Auth Service addition

---
+++ src/main/java/rw/rca/ac/airlines/reserve/auth/AuthService.java
@@ -0,0 +1,11 @@
+package rw.rca.ac.airlines.reserve.auth;
+
+public class AuthService
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Username: ");
+        System.out.println("Email: ");
+
+    }
+}

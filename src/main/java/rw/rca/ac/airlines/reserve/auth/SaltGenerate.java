4c647a226ffb39335b3e408456cf93b09a389290
Manzi Cedrick <cedrickmanzii0@gmail.com>
2023-01-28 21:31:31 +0200

    [add] Signin Password encryption

--- src/main/java/rw/rca/ac/airlines/reserve/auth/SaltGenerate.java
+++ src/main/java/rw/rca/ac/airlines/reserve/auth/SaltGenerate.java
@@ -9,10 +9,16 @@ public class SaltGenerate
     private static final String characterEnc = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     private static final int iterations =1000;
     private static final int finalLength = 25;
-    private String getSaltValue (int length){
+    private static String getSaltValue (int length){
         StringBuilder finalVal = new StringBuilder(length);
         for(int i=0;i<length;i++){
-
+            finalVal.append(characterEnc.charAt(random.nextInt(characterEnc.length())));
         }
+        return new String(finalVal);
+    }
+    private String generateSecurePassword(String password,String salt)
+    {
+        String finalVal = null;
+        return finalVal;
     }
 }
---
+++ src/main/java/rw/rca/ac/airlines/reserve/auth/SignIn.java
@@ -0,0 +1,13 @@
+package rw.rca.ac.airlines.reserve.auth;
+
+public class SignIn
+{
+    private String email;
+    private String password;
+    private String encryptPassword() {
+        return password;
+    }
+    private void loginFunctionality () {
+//        String saltValue =
+    }
+}
